b1b4a0a81a63d15f509640236d618b031cbfbb7f
Marc M. Dion <mcmdion@yahoo.com>
2019-10-08 21:29:25 +1300

    Updates

---
+++ precache-manifest.16f54f3456ff73e3705f88dbda5b74bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9618073c76c609296c85bec23023396a",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "e614c2f5b6f4c27b1431",
+    "url": "/SmartBrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "920a23406ac96d4af7b3",
+    "url": "/SmartBrain/static/css/main.ebc84e85.chunk.css"
+  },
+  {
+    "revision": "e614c2f5b6f4c27b1431",
+    "url": "/SmartBrain/static/js/2.6c2f154f.chunk.js"
+  },
+  {
+    "revision": "920a23406ac96d4af7b3",
+    "url": "/SmartBrain/static/js/main.00b82f45.chunk.js"
+  },
+  {
+    "revision": "29a8a07f1e1417575e0b",
+    "url": "/SmartBrain/static/js/runtime-main.23d91a5f.js"
+  }
+]);
